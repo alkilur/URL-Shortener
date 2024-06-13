55a00e51bbe04f0f3da65f84fbad848f827dcee8
alkilur <alk.migliore@gmail.com>
2024-06-13 17:49:01 +0400

    test for redirect handler

---
+++ internal/http-server/handlers/redirect/redirect_test.go
@@ -0,0 +1,76 @@
+package redirect_test
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"url-shortener/internal/http-server/handlers/redirect"
+	"url-shortener/internal/http-server/handlers/redirect/mocks"
+	"url-shortener/internal/lib/logger/slogdiscard"
+)
+
+func GetRedirect(url string) (string, error) {
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusFound {
+		return "", fmt.Errorf("%w: %d", errors.New("invalid status code"), resp.StatusCode)
+	}
+
+	return resp.Header.Get("Location"), nil
+}
+
+func TestRedirectHandler(t *testing.T) {
+	cases := []struct {
+		name      string
+		alias     string
+		url       string
+		respError string
+		mockError error
+	}{
+		{
+			name:  "Success",
+			alias: "test_alias",
+			url:   "https://www.google.com/",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			urlGetterMock := mocks.NewURLGetter(t)
+
+			if tc.respError == "" || tc.mockError != nil {
+				urlGetterMock.On("GetURL", tc.alias).
+					Return(tc.url, tc.mockError).Once()
+			}
+
+			r := chi.NewRouter()
+			r.Get("/{alias}", redirect.NewRedirect(slogdiscard.NewDiscardLogger(), urlGetterMock))
+
+			ts := httptest.NewServer(r)
+			defer ts.Close()
+
+			redirectedToURL, err := GetRedirect(ts.URL + "/" + tc.alias)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.url, redirectedToURL)
+		})
+	}
+}
